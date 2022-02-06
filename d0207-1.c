b56877dc79b4a07370ee886c26710d1eaa8dc0f8
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-07 02:09:33 +0900

    ㅋㅋㅋㅋ

--- d0207-1.c
+++ d0207-1.c
@@ -3,20 +3,23 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+int month[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+const char* dates[][7] = { {"SUN"}, {"MON"}, {"TUE"}, {"WED"}, {"THU"}, {"FRI"}, {"SAT"} };
 char* solution(int a, int b) {
-    char* arr[] = {"FRI", "SAT", "SUN","MON","TUE","WED","THU"};
-    // 리턴할 값은 메모리를 동적 할당해주세요.
-    char* answer = (char*)malloc(sizeof(arr));
-    
-    int final_day[20] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-    int total_day = 0;
 
-    for (int i = 0; i < a - 1; i++) {
-        total_day += final_day[i];
+    int date = 0;
+    int day;
+    for (int i = 0; i < a - 1; i++)
+    {
+        date += month[i];
     }
-    
-    total_day += b - 1;
-    answer = arr[total_day % 7];
+
+    date += b;
+
+    day = (date % 7 + 4) % 7;
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    char* answer = (char*)malloc(3 * sizeof(char));
+    strcpy(answer, dates[day][0]);
     
     return answer;
 }
