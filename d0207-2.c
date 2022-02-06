8a75ab789191273ce16bf4e1c51501b4183753d1
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-07 01:58:41 +0900

    hh

---
+++ d0207-2.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 반환 값은 없지만, 파라미터(입력값)은 있는 함수 선언
+void is_leap(int year);
+int main(void)
+{
+	int year;
+	printf("enter year : ");
+	scanf("%d", &year);
+
+	is_leap(year);
+
+	return 0;
+}
+
+//setting function
+void is_leap(int year)
+{
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	{
+		printf("%d year is 366 days. \n", year); //
+	}
+	else {
+		printf("%d year is 365 days . \n", year); // 
+	}
+}
