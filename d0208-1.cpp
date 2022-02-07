d45489167ff2ea52ca8db72c4f08b34078319abb
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-08 01:01:55 +0900

    헐

---
+++ d0208-1.cpp
@@ -0,0 +1,20 @@
+/////https://programmers.co.kr/learn/courses/30/lessons/42746?language=cpp
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(int a, int b) {
+	string s1 = to_string(a) + to_string(b);
+	string s2 = to_string(b) + to_string(a);
+	return s1 > s2;
+}
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	sort(numbers.begin(), numbers.end(), cmp);
+	for (auto& i : numbers)
+		answer += to_string(i);
+	return answer[0] == '0' ? "0" : answer;
+}
