afa88d93c069401adf759b1008f5a9e7bfc805d0
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-10 16:57:27 +0900

    정올 배낭 채우기

---
+++ d0210-2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+int W[1010];
+int P[1010];
+
+int DP[1010][10010];
+
+int main() {
+    scanf("%d%d", &N, &K);
+    for (int i = 1; i <= N; i++) {
+        scanf("%d %d", &W[i], &P[i]);
+    }
+    ////solution
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= K; j++) {
+            //DP[i][j] 관련 점화식
+            if (j < W[i])
+                DP[i][j] = DP[i - 1][j];
+            else
+                DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - W[i]] + P[i]);
+        }
+    }
+
+    printf("%d", DP[N][K]);
+}
+////http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=561&sca=3060
